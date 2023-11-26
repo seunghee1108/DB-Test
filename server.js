597d81aa8e0dd43f441408b507c9bafb9ced7828
승희 <bangseunghee1108@gmail.com>
2023-11-26 19:35:15 +0900

    DB에 작성한 data 가져오기 성공

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express();
 
 const PORT = 3000;
 
-const { MongoClient } = require('mongodb')
+app.use(express.static(__dirname + '/public'));
+
+const { MongoClient } = require('mongodb');
+const { default: test } = require('node:test');
 
 let db
-// 사이트에서 Connet에 링크 있음(id, pw 작성)
+// 사이트에서 Connect에 링크 있음(id, pw 작성)
 const url = 'mongodb+srv://admin:sh123@cluster0.lfkcymr.mongodb.net/?retryWrites=true&w=majority'
 // mongoDB 접속
 new MongoClient(url).connect().then((client)=>{
@@ -20,6 +23,16 @@ new MongoClient(url).connect().then((client)=>{
   console.log(err)
 })
 
+// app.get('/', (req, res) => {
+//   res.send('안녕하세요');
+// });
+
 app.get('/', (req, res) => {
-  res.send('안녕하세요');
-});
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/test', async(req, res) => {
+  const test = await db.collection('data').find().toArray()
+  console.log(test)
+  res.send('DB내용')
+})

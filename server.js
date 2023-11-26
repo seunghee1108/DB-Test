6ebdbecd067608bbf405145e15933bd279db8af7
승희 <bangseunghee1108@gmail.com>
2023-11-26 19:16:31 +0900

    DB연결 성공

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const app = express();
 
 const PORT = 3000;
 
+const { MongoClient } = require('mongodb')
+
+let db
+const url = 'mongodb+srv://admin:sh123@cluster0.lfkcymr.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  db = client.db('test')
+}).catch((err)=>{
+  console.log(err)
+})
+
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
  });

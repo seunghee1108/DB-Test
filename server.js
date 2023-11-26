e68b6995e435432abad47b24e44535016ddd241d
승희 <bangseunghee1108@gmail.com>
2023-11-26 19:24:42 +0900

    MongoDB 접속

--- server.js
+++ server.js
@@ -6,18 +6,19 @@ const PORT = 3000;
 const { MongoClient } = require('mongodb')
 
 let db
+// 사이트에서 Connet에 링크 있음(id, pw 작성)
 const url = 'mongodb+srv://admin:sh123@cluster0.lfkcymr.mongodb.net/?retryWrites=true&w=majority'
+// mongoDB 접속
 new MongoClient(url).connect().then((client)=>{
   console.log('DB연결성공')
+  // 내가 생성한 project name
   db = client.db('test')
+  app.listen(PORT, () => {
+    console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
+   });
 }).catch((err)=>{
   console.log(err)
 })
-
-app.listen(PORT, () => {
-  console.log(`서버가 http://localhost:${PORT} 에서 실행 중입니다.`);
- });
-
 
 app.get('/', (req, res) => {
   res.send('안녕하세요');
